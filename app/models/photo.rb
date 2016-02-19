01cc73ee1293e0c3adebb09283ea856c2ec52867
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-19 17:41:30 +0800

    Set up photo model validation (Imposed)

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,9 @@
 class Photo < ActiveRecord::Base
+	has_attached_file :image_file
+  # Validate content type
+  validates_attachment_content_type :image_file, content_type: /\Aimage/
+  # Validate filename
+  validates_attachment_file_name :image_file, matches: [/png\Z/, /jpe?g\Z/]
+  # Explicitly do not validate
+  do_not_validate_attachment_file_type :image_file
 end
