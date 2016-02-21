3b3d2eee2d4ecc3b5f907c2993492fda8a83d21a
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-21 14:22:44 +0800

    Modify devise parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+	    user_params.permit(:email)
+	  end
+
+	  devise_parameter_sanitizer.permit(:account_update) do |user_params|
+	  	user_params.permit(:email, :avatar, :password, :password_confirmation, :current_password)
+	  end
+  end
 end
