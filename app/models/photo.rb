049746edc88682b85baaa04ec36dddceb840a346
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-21 22:40:23 +0800

    Modify the models file

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,3 @@
 class Photo < ActiveRecord::Base
-	validates :title, :description, :image_file, presence: true
-
-	has_attached_file :image_file
-  # Validate content type
-  validates_attachment_content_type :image_file, content_type: /\Aimage/
-  # Validate filename
-  validates_attachment_file_name :image_file, matches: [/png\Z/, /jpe?g\Z/]
-  # Explicitly do not validate
-  do_not_validate_attachment_file_type :image_file
+	validates :title, :description, :url, presence: true
 end
