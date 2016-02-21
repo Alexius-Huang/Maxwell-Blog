366dfccbe9f8246d81ba0b8d7f5a55d2b4680f33
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-21 22:36:42 +0800

    Remove the columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221064427) do
+ActiveRecord::Schema.define(version: 20160221143219) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,10 +26,6 @@ ActiveRecord::Schema.define(version: 20160221064427) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "name"
     t.text     "about"
   end
@@ -47,12 +43,8 @@ ActiveRecord::Schema.define(version: 20160221064427) do
   create_table "photos", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "image_file_file_name"
-    t.string   "image_file_content_type"
-    t.integer  "image_file_file_size"
-    t.datetime "image_file_updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
