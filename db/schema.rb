18933da85cdc91ad3d6ce639afeacd7d25849fa7
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-21 22:39:25 +0800

    Add url column to photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221143219) do
+ActiveRecord::Schema.define(version: 20160221143826) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160221143219) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "url"
   end
 
 end
