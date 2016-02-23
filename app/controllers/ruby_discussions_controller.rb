66a7df793b3a13e2f2e1d4c14dfc740d105f4665
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-23 20:43:59 +0800

    Set up rails controller

--- app/controllers/ruby_discussions_controller.rb
+++ app/controllers/ruby_discussions_controller.rb
@@ -1,2 +1,51 @@
 class RubyDiscussionsController < ApplicationController
+	
+	def index
+		@discussions = RubyDiscussion.order("updated_at DESC")
+	end
+
+	def new
+		@discussion = RubyDiscussion.new
+	end
+
+	def create
+		@discussion = RubyDiscussion.create(discussion_params)
+
+		if @discussion.save
+			redirect_to ruby_discussions_path
+		else
+			render :new
+		end
+	end
+
+	def show
+		@discussion = RubyDiscussion.find(params[:id])
+	end
+
+	def edit
+		@discussion = RubyDiscussion.find(params[:id])
+	end
+
+	def update
+		@discussion = RubyDiscussion.find(params[:id])
+
+		if @discussion.update(discussion_params)
+			redirect_to ruby_discussion_path(@discussion)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@discussion = RubyDiscussion.find(params[:id])
+		@discussion.destroy
+		redirect_to ruby_discussions_path
+	end
+
+	private
+
+	def discussion_params
+		params.require(:ruby_discussion).permit(:title, :description)
+	end
+
 end
