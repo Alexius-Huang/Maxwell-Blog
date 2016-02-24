9ae53e7fe78d3530396f8169019eb13d3d470247
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-24 16:25:42 +0800

    Modify gitignore file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,5 @@ class HomeController < ApplicationController
   end
 
   def computer_science
-  	@programming_article = ProgrammingBasic.last
-  	@ruby_article = RubyDiscussion.last
-  	@rails_article = RailsDiscussion.last
-  	@ruby_gem_article = RubyGem.last
   end
 end
