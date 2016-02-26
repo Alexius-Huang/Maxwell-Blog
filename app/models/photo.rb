3e52ba8b38b16739fe52529d0ae562f1e90e1ae5
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-02-26 10:20:14 +0800

    Set up polymorphic association between models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ActiveRecord::Base
+	has_many :comments, as: :commentable
 	validates :title, :description, :url, presence: true
 end
--- app/models/ruby_discussion.rb
+++ app/models/ruby_discussion.rb
@@ -1,2 +1,3 @@
 class RubyDiscussion < ActiveRecord::Base
+	has_many :comments, as: :commentable
 end
